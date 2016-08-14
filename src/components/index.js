603fe0f8a97b3785c261330158edff349cf29531
Jarryd Goodman <jarryd999@gmail.com>
2016-08-14 15:49:53 -0700

    added to exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import App from './App';
 import Tile from './Tile.js';
 import TopBar from './TopBar.js';
 import Column from './Column.js';
+import ColumnOuter from './ColumnOuter.js';
 
-export { App, Tile, TopBar, Column };
+export { App, Tile, TopBar, Column, ColumnOuter };

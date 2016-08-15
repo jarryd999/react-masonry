8cf3ae68675b9aef03e072f087cb1b74385a6c91
Jarryd Goodman <jarryd999@gmail.com>
2016-08-14 19:23:16 -0700

    added infinite scroll and resizing

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import thunkMiddleware from 'redux-thunk';
 import * as reducers from './reducers/index.js';
 
 import { App } from './components';
-import { ColumnOuterContainer } from './containers';
-import { initializeSDK } from './actions';
+import { ColumnOuterContainer, TopbarContainer } from './containers';
+import { initializeSDK, setColumnCount, addImages, fetchImages } from './actions';
 
 import './index.scss';
 
@@ -16,13 +16,48 @@ const store = createStore(
 	applyMiddleware(thunkMiddleware)
 );
 
+
 store.dispatch(initializeSDK());
+
+determineColumnCount(store);
+
+window.onresize = determineColumnCount;
+window.onscroll = infiniteScrollLoader;
+
 
 ReactDOM.render(
     <Provider store={store}>
         <App >
+        	<TopbarContainer/>
             <ColumnOuterContainer/>
         </App>
     </Provider>,
     document.getElementById('root')
 );
+
+
+
+
+// Helper functions for window events
+
+function determineColumnCount(){
+	if (window.innerWidth <= 800)
+		store.dispatch(setColumnCount(2));
+	else
+		store.dispatch(setColumnCount(3));
+
+	store.dispatch(addImages(store.getState().imageState.images));
+}
+
+function infiniteScrollLoader(){
+	if (getScrollPercent() >= 95 && !store.getState().imageState.isFetching){
+		store.dispatch(fetchImages());
+	}
+}
+function getScrollPercent() {
+    var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return h[st]||b[st] / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+}
